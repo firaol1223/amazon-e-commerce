d6bef7bf35dee88978385378290e326145c49d6f
firaol1223 <firaolandarsa22@gmail.com>
2024-07-27 22:16:24 +0300

    update on loader

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of firbase functions
-  // baseURL: "http://127.0.0.1:5001/e-commerce-99ed4/us-central1/api",
+  baseURL: "http://127.0.0.1:5001/e-commerce-99ed4/us-central1/api",
   // deployed version of amazon server on render.com
-  baseURL: "https://amazon-api-deploy-9x3v.onrender.com/",
+  // baseURL: "https://amazon-api-deploy-9x3v.onrender.com/",
 });
 
-export {axiosInstance}
+export { axiosInstance };
--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { productUrl } from "../../Api/endPoints";
 import ProductCard from "../../components/Product/ProductCard";
+import Loader from "../../components/Loader/Loader";
 
 function Results() {
     const [results, setResults] = useState([]);
